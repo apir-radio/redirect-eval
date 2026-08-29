6d0266eaff10ced0e634bbfd947a0aa55d7bfcd3
Max le thorax <apir.radiologie@gmail.com>
2026-08-29 10:47:37 +0200

    Remplace les vues filtrées par des onglets précis

--- resultats/actuel/destination.js
+++ resultats/actuel/destination.js
@@ -1,1 +1,1 @@
-const APIR_DESTINATION_URL = "https://docs.google.com/spreadsheets/d/1uzCGSF5sxSC43ywziG-tRtyZ3GNC2a8h3OiTW-GzAaM/edit?gid=532696545#gid=532696545&fvid=955824260";
+const APIR_DESTINATION_URL = "https://docs.google.com/spreadsheets/d/1uzCGSF5sxSC43ywziG-tRtyZ3GNC2a8h3OiTW-GzAaM/edit?gid=337811311#gid=337811311";
